a025d4d5ced27b20730af3cce1d2c8859d6d3b55
Abysscope <abysscope@gmail.com>
2021-11-01 23:59:42 +0300

    parse html

--- begining/backend/parser.py
+++ begining/backend/parser.py
@@ -1,36 +1,33 @@
 from html.parser import HTMLParser
 import codecs
 import os
-import re
+import pprint
 from bs4 import BeautifulSoup
 
+
 def parse(html_file='/Users/Abysscope/Documents/example/4_1.htm'):
-    dirf, namef = os.path.split(html_file)
-    write_file = '{}/{}utf.html'.format(dirf, namef.split('.')[0])
+    """Parse HTML page for table data"""
 
     with open(html_file, 'r', encoding='cp1251') as read:
         soup = BeautifulSoup(read, 'html.parser')
-        #with open(write_file, 'w', encoding='utf-8') as write:
-        #    write.write(read.read())
 
-    #read_file = codecs.decode(bytes(read_file), encoding='utf-8')
-    regex = re.compile(r'\s*')
-    regex_string = re.compile(r'\s*(\W.*)')
-    soup_table = BeautifulSoup(str(soup.find_all('table', width='680')), 'html.parser')
-    tr_list = soup_table.find_all('tr')
-
+    table_soup = BeautifulSoup(str(soup.find_all('table', width='718')), 'html.parser') # width='680':page '4' width='718':page '6'
+    tr_list = table_soup.find_all('tr')
+    td_data = []
     for tr in tr_list:
-        #td_str = td.text.replace('\n', '')
-        #if not regex.fullmatch(td_str):
         td_list = BeautifulSoup(str(tr), 'html.parser').find_all('td')
-        if len(td_list) == 11:
-            for td in td_list:
-                if not len(str(td.text).strip()) == 0:
-                    print(str(td.text).strip())
+        if len(td_list) == 11 and not (str(td_list[3].text).strip() ==
+                            str(td_list[5].text).strip() ==
+                            str(td_list[7].text).strip() ==
+                            str(td_list[9].text).strip() == ''):
+            td_data.append([str(td_list[1].text).strip(),
+                            str(td_list[3].text).strip(),
+                            str(td_list[5].text).strip(),
+                            str(td_list[7].text).strip(),
+                            str(td_list[9].text).strip()])
+    pprint.pprint(td_data)
 
-def has_attr_in_tag(tag):
-    return tag.has_attr("width") and tag.has_attr("")
 
 if __name__ == '__main__':
-    parse('/Users/Abysscope/Documents/example/4_1.htm')
+    parse('/Users/Abysscope/Documents/example/6_all.htm')
     pass
