2e5e3b4db0483e589e9ac1f5cd2e83cce23a0457
NutsHubs <abysscope@gmail.com>
2023-05-23 11:50:47 +0300

    changes

--- begining/begining/settings.py
+++ begining/begining/settings.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-#add to github
-import abc
 
 import os
 import json
